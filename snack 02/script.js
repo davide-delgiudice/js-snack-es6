1112d0536ccf2bb7216fc918352e0aa09c1814e4
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-28 17:28:36 +0100

    [UPD] add ex 02 note - Esercizio Terminato

--- snack 02/script.js
+++ snack 02/script.js
@@ -1,3 +1,5 @@
+// creo un array di oggetti contenente una serie di squadre di calcio, con parametri il loro nome, punti fatti e falli subiti
+// punti e falli saranno di base 0
 const teams = [
     {
         name: "Juventus",
@@ -31,6 +33,7 @@ const teams = [
     }
 ]
 
+// creo due funzioni per generare numeri randomici
 const generateRandomPoints = () => {
     return Math.floor(Math.random() * 10) + 1;
 }
@@ -39,23 +42,30 @@ const generateRandomFoul = () => {
     return Math.floor(Math.random() * 30) + 1;
 }
 
+// tramite un ciclo for assegno i valori randomici ad ogni parametro points e foul dei team
 for (let i=0; i<teams.length; i++){
     teams[i].points = generateRandomPoints();
     teams[i].foul = generateRandomFoul();
 }
 
+// stampo l'array teams
 console.log(teams);
 
+// creo un array vuoto che dovrà contenere solo i nomi e i falli subiti dei team
 const detailsTeams = [];
 
+// tramite un ciclo for assegno i valori name e foul ad una variabile di appoggio per poi inserirli nel nuovo array vuoto
 for (let i=0; i<teams.length; i++){
     
+    // creo la variabile di appoggio con all'interno i valori
     let nameAndFoul = {
         name: teams[i].name,
         foul: teams[i].foul,
     };
-    
+
+    // pusho i valori nell'array
     detailsTeams.push(nameAndFoul);
 }
 
+// stampo il nuovo array
 console.log(detailsTeams);
