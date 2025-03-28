b5caf6c234c101a8e9ca58c00259e56fae5f6377
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-28 15:55:18 +0100

    [UPD] create teams objects

---
+++ snack 02/script.js
@@ -0,0 +1,27 @@
+const teams = [
+    {
+        name: "Juventus",
+        points: 0,
+        foul: 0
+    },
+    {
+        name: "Milan",
+        points: 0,
+        foul: 0
+    },
+    {
+        name: "Inter",
+        points: 0,
+        foul: 0
+    },
+    {
+        name: "Sampdoria",
+        points: 0,
+        foul: 0
+    },
+    {
+        name: "Lazio",
+        points: 0,
+        foul: 0
+    },
+]
