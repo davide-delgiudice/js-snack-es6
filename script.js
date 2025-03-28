8e69b7adfd615cfaae8a07fe7c331fc46755c06d
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-28 15:12:32 +0100

    [UPD] create bikes object

---
+++ script.js
@@ -0,0 +1,22 @@
+const bikes = [
+    {
+        name: "Fiido C21",
+        weight: 17.5
+    },
+    {
+        name: "Triban",
+        weight: 10.8
+    },
+    {
+        name: "Van Rysel",
+        weight: 9
+    },
+    {
+        name: "Aeroad",
+        weight: 14
+    },
+    {
+        name: "Ultimate",
+        weight: 13.4
+    }
+]
